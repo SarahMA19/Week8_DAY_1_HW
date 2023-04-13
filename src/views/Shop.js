57f9dfd6d4e51ca60a199f5bbabbf6175d922319
SarahMA19 <sarahgreisen@gmail.com>
2023-04-13 15:15:14 -0700

    connected

--- src/views/Shop.js
+++ src/views/Shop.js
@@ -1,7 +1,27 @@
-
+import {useEffect, useState} from "react";
+import axios from "axios";
 
 
 const Shop = () => {
+    
+    useEffect(() => { console.log('Shop component has rendered')});
+    const getProductData = async () => {
+        let response = await axios.get('http://127.0.0.1:5000/api/products');
+        return response.status === 200 ? response.data : null
+    }
+    const loadProductData = async () => {
+        let data = await getProductData();
+        console.log(data, typeof data)
+        setProducts(data.data)
+
+    }
+    const [products, setProducts] = useState(() => loadProductData());
+    
+    
+    
+    const web_url = "";
+    const local_url = "";
+
     return (
         <div>
             <h1> SHOP </h1>
